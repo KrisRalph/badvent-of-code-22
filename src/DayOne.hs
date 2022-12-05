4cef633055d78b6752a2131bc2260751d388a42d
Kris Ralph <kris.ralph45@gmail.com>
2022-12-05 14:23:44 +0000

    just prep import list

--- src/DayOne.hs
+++ src/DayOne.hs
@@ -1,26 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
-
 module DayOne (main) where
 
-import Control.Monad (join)
-import Data.Function (on)
-import Data.List (maximumBy)
-import Data.Maybe (fromMaybe)
-import Data.Text (Text)
-import Data.Text qualified as Text
-import Data.Text.IO qualified as Text.IO
-import Text.Read
-
-elfList :: IO Text
-elfList = Text.IO.readFile "./files/DayOneElfList.txt"
-
-dayOneQ :: Text -> [Int]
-dayOneQ t = maximumBy (compare `on` sum) readInts
-  where
-    splitLines = filter ((/= 0) . Text.length) . Text.splitOn "\n" <$> Text.splitOn "\n\n" t
-    readableStrings = fmap Text.unpack <$> splitLines
-    readInts = fmap (read @Int) <$> readableStrings
-
 main :: IO ()
-main = elfList >>= print . dayOneQ
+main = putStrLn "DayOne"
