4e4bb9af5bb91a6bfe7f7f0674b67b91c5750ffc
Kristofer Ralph <kris.ralph45@gmail.com>
2022-12-07 13:01:56 +0000

    bin partial read

--- src/DayOne.hs
+++ src/DayOne.hs
@@ -6,21 +6,22 @@ module DayOne (main) where
 import Control.Monad (join)
 import Data.Function (on)
 import Data.List (maximumBy)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (mapMaybe)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text.IO
-import Text.Read
+import Text.Read (readMaybe)
 
 elfList :: IO Text
 elfList = Text.IO.readFile "./files/DayOneElfList.txt"
 
-dayOneQ :: Text -> [Int]
-dayOneQ t = maximumBy (compare `on` sum) readInts
+dayOneQA :: Text -> [Int]
+dayOneQA t = maximumBy (compare `on` sum) readInts
   where
     splitLines = filter ((/= 0) . Text.length) . Text.splitOn "\n" <$> Text.splitOn "\n\n" t
     readableStrings = fmap Text.unpack <$> splitLines
-    readInts = fmap (read @Int) <$> readableStrings
+    readInts = mapMaybe (readMaybe @Int) <$> readableStrings
 
 main :: IO ()
-main = elfList >>= print . dayOneQ
+main =
+  elfList >>= print . dayOneQA
